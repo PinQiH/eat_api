82b87ce5a21febb38f2be8c6f8a0ce344d795b49
PinQiH <cassie34131@gmail.com>
2024-01-11 13:59:55 +0800

    api-init

--- controllers/apisController.js
+++ controllers/apisController.js
@@ -1,3 +1,70 @@
+const models = require("../models")
+
 const apisController = {}
+
+// 用戶註冊
+apisController.registerUser = async (req, res) => {
+  // TODO: 實現用戶註冊邏輯
+}
+
+// 用戶登錄
+apisController.loginUser = async (req, res) => {
+  // TODO: 實現用戶登錄邏輯
+}
+
+// 獲取用戶資訊
+apisController.getUserInfo = async (req, res) => {
+  // TODO: 實現獲取用戶資訊邏輯
+}
+
+// 更新用戶資訊
+apisController.updateUserInfo = async (req, res) => {
+  // TODO: 實現更新用戶資訊邏輯
+}
+
+// 獲取所有食物類別
+apisController.getAllCategories = async (req, res) => {
+  // TODO: 實現獲取所有食物類別邏輯
+}
+
+// 添加新的食物類別
+apisController.addCategory = async (req, res) => {
+  // TODO: 實現添加食物類別邏輯
+}
+
+// 獲取用戶黑名單
+apisController.getUserBlacklist = async (req, res) => {
+  // TODO: 實現獲取用戶黑名單邏輯
+}
+
+// 添加食物到黑名單
+apisController.addToBlacklist = async (req, res) => {
+  // TODO: 實現添加食物到黑名單邏輯
+}
+
+// 從黑名單中移除食物
+apisController.removeFromBlacklist = async (req, res) => {
+  // TODO: 實現從黑名單移除食物邏輯
+}
+
+// 獲取用戶的轉盤歷史
+apisController.getSpinnerHistory = async (req, res) => {
+  // TODO: 實現獲取轉盤歷史邏輯
+}
+
+// 添加轉盤歷史記錄
+apisController.addSpinnerHistory = async (req, res) => {
+  // TODO: 實現添加轉盤歷史邏輯
+}
+
+// 獲取用戶自定義食物類別
+apisController.getCustomCategories = async (req, res) => {
+  // TODO: 實現獲取用戶自定義食物類別邏輯
+}
+
+// 添加用戶自定義食物類別
+apisController.addCustomCategory = async (req, res) => {
+  // TODO: 實現添加用戶自定義食物類別邏輯
+}
 
 module.exports = apisController
--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,42 @@ const express = require("express")
 const router = express.Router()
 const apisController = require("../controllers/apisController")
 
+// 用戶管理路由
+router.post("/api/users/register", apisController.registerUser)
+router.post("/api/users/login", apisController.loginUser)
+router.get("/api/users/:userId", apisController.getUserInfo)
+router.put("/api/users/:userId", apisController.updateUserInfo)
+
+// 食物類別管理路由
+router.get("/api/categories", apisController.getAllCategories)
+router.post("/api/categories", apisController.addCategory)
+
+// 用戶黑名單管理路由
+router.get("/api/users/:userId/blacklist", apisController.getUserBlacklist)
+router.post("/api/users/:userId/blacklist", apisController.addToBlacklist)
+router.delete(
+  "/api/users/:userId/blacklist/:categoryId",
+  apisController.removeFromBlacklist
+)
+
+// 轉盤歷史記錄路由
+router.get(
+  "/api/users/:userId/spinner-history",
+  apisController.getSpinnerHistory
+)
+router.post(
+  "/api/users/:userId/spinner-history",
+  apisController.addSpinnerHistory
+)
+
+// 用戶自定義食物類別路由
+router.get(
+  "/api/users/:userId/custom-categories",
+  apisController.getCustomCategories
+)
+router.post(
+  "/api/users/:userId/custom-categories",
+  apisController.addCustomCategory
+)
+
 module.exports = router
